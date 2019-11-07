a3efb15aa552d5f73eee7ac03120b2c0243f6425
HeechanYang <yhc944@gmail.com>
2019-11-07 13:58:19 +0900

    Controller 분리 (`HelloController`, `CalculatorController`)

--- src/main/java/com/example/demo/controller/CalculatorController.java
+++ src/main/java/com/example/demo/controller/CalculatorController.java
@@ -1,16 +1,13 @@
-package com.example.demo;
+package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloController {
-
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hello, Spring!";
-    }
+@RequestMapping("/calculator")
+public class CalculatorController {
 
     @GetMapping("/sum")
     public int sum(@RequestParam int num1, @RequestParam int num2) {
@@ -31,5 +28,4 @@ public class HelloController {
     public int div(@RequestParam int num1, @RequestParam int num2) {
         return num1 / num2;
     }
-
 }
