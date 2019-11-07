e50475b8a0c367a36c0ea405549b679f8b02ca8e
HeechanYang <yhc944@gmail.com>
2019-11-07 12:53:28 +0900

    간단한 API 구현 (Sum)

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,4 +11,10 @@ public class HelloController {
     public String hello() {
         return "Hello, Spring!";
     }
+
+    @GetMapping("/sum")
+    public int sum(@RequestParam int num1, @RequestParam int num2) {
+        return num1 + num2;
+    }
+
 }
